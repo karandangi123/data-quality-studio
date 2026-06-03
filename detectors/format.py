c7a9ca09c048994ce915df95a62827431445c288
karandangi123 <karandangi1867@gmail.com>
2026-06-03 22:15:32 +0530

    Fix DuckDB Catalog Error related to r'' string syntax

--- detectors/format.py
+++ detectors/format.py
@@ -8,11 +8,11 @@ class FormatDetector(BaseDetector):
         
         # Check signup_date format (YYYY-MM-DD)
         if 'signup_date' in raw_cols:
-            invalid_dates = conn.execute(f"""
+            invalid_dates = conn.execute(rf"""
                 SELECT COUNT(*) FROM {raw_table}
                 WHERE signup_date IS NOT NULL 
                 AND CAST(signup_date AS VARCHAR) != ''
-                AND NOT regexp_matches(CAST(signup_date AS VARCHAR), r'^\d{{4}}-\d{{2}}-\d{{2}}$')
+                AND NOT regexp_matches(CAST(signup_date AS VARCHAR), '^\d{{4}}-\d{{2}}-\d{{2}}$')
             """).fetchone()[0]
             
             if invalid_dates > 0:
@@ -26,11 +26,11 @@ class FormatDetector(BaseDetector):
                 
         # Check phone format (+91-XXXXX...)
         if 'phone' in raw_cols:
-            invalid_phones = conn.execute(f"""
+            invalid_phones = conn.execute(rf"""
                 SELECT COUNT(*) FROM {raw_table}
                 WHERE phone IS NOT NULL 
                 AND CAST(phone AS VARCHAR) != ''
-                AND NOT regexp_matches(CAST(phone AS VARCHAR), r'^\+\d{{2}}-\d{{10}}$')
+                AND NOT regexp_matches(CAST(phone AS VARCHAR), '^\+\d{{2}}-\d{{10}}$')
             """).fetchone()[0]
             
             if invalid_phones > 0:
@@ -39,8 +39,7 @@ class FormatDetector(BaseDetector):
                     severity="Warning",
                     column="phone",
                     description=f"Found {invalid_phones} phones not matching +XX-XXXXXXXXXX format.",
-                    # Removes all non-numeric characters and formats as +91-XXXXXXXXXX (assuming Indian numbers for this assignment)
-                    sql_fix=f"SELECT * REPLACE ('+91-' || RIGHT(regexp_replace(CAST(phone AS VARCHAR), '[^0-9]', '', 'g'), 10) AS phone) FROM {raw_table};"
+                    sql_fix=rf"SELECT * REPLACE ('+91-' || RIGHT(regexp_replace(CAST(phone AS VARCHAR), '[^0-9]', '', 'g'), 10) AS phone) FROM {raw_table};"
                 ))
                 
         return issues
